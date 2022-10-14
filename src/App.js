733035781546df734527b80779ece7b7e052bd86
Paweł Potempa <pwpotempa@gmail.com>
2022-10-14 02:17:23 +0200

    feat: Redirect to "/" from blocked tasks
    - Disable ability to click a blocked link
    - Implement redirect in case of manual url input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import Dashboard from "./components/Dashboard/Dashboard";
 import Layout from "./components/Layout/Layout";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import tasksData from "./data/tasks.json";
 
 function App() {
+  const { taskSlug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const currentTask = tasksData.find((obj) => obj.slug === taskSlug);
+    if (currentTask.status === "blocked") {
+      navigate(`/${tasksData[0].slug}`);
+    }
+  }, [navigate, taskSlug]);
+
   return (
     <Layout>
       <Dashboard />
