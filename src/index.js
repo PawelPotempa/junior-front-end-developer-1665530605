e5db039690253972f062b4d63d5b59e369a5c1d7
Paweł Potempa <pwpotempa@gmail.com>
2022-10-14 02:15:22 +0200

    feat: Set up react-router-dom
    - Swap anchor tags to NavLink
    - Set up slug for TasksList items
    - Implement redirect from home path

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import tasksData from "./data/tasks.json";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:taskSlug/" element={<App />} />
+        <Route
+          path="*"
+          element={<Navigate to={`/${tasksData[0].slug}`} replace />}
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
